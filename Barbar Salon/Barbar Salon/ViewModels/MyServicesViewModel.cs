1e054ebb6862eca8eb975b37412e2df4cebb1237
zaidmelhem <68277240+zaidmelhem@users.noreply.github.com>
2022-04-05 16:21:19 +0300

    Update MyServicesViewModel.cs

--- Barbar Salon/Barbar Salon/ViewModels/MyServicesViewModel.cs
+++ Barbar Salon/Barbar Salon/ViewModels/MyServicesViewModel.cs
@@ -1,10 +1,25 @@
-﻿using System;
+using Barbar_Salon.Models;
+using Barbar_Salon.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Barbar_Salon.ViewModels
 {
     public class MyServicesViewModel
     {
+        FireBaseHaloHair firebase;
+
+        public ObservableCollection<MyServicesModel> Services { get; set; }
+
+
+        public MyServicesViewModel()
+        {
+            firebase = new FireBaseHaloHair();
+            Services = new ObservableCollection<MyServicesModel>();
+            Services = firebase.getServices();
+
+        }
     }
 }
