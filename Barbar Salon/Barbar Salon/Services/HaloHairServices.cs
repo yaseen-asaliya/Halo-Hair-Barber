48f97838eeccae199c4e4b54cdd0be1284d2bbe6
zaidmelhem <68277240+zaidmelhem@users.noreply.github.com>
2022-04-05 16:22:45 +0300

    Update HaloHairServices.cs

--- Barbar Salon/Barbar Salon/Services/HaloHairServices.cs
+++ Barbar Salon/Barbar Salon/Services/HaloHairServices.cs
@@ -9,4 +9,10 @@ namespace Barbar_Salon.Services
     {
         FirebaseClient firebaseClient = new FirebaseClient("https://halo-hair-676ed-default-rtdb.firebaseio.com");
     }
+     public ObservableCollection<MyServicesModel> getServices()
+        {
+            var servicesData = firebaseClient.Child("Services").AsObservable<MyServicesModel>().AsObservableCollection();
+
+            return servicesData;
+        }
 }
