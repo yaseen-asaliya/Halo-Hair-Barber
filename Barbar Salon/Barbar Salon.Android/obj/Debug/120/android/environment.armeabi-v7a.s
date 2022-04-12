1e528c8993c46ed4c2a0c5435eb5a200e4b80c8e
Yaseen asaliya <59315877+yaseen-asaliya@users.noreply.github.com>
2022-04-12 04:08:26 +0300

    test

--- Barbar Salon/Barbar Salon.Android/obj/Debug/120/android/environment.armeabi-v7a.s
+++ Barbar Salon/Barbar Salon.Android/obj/Debug/120/android/environment.armeabi-v7a.s
@@ -99,7 +99,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"4118ad40-47b2-4670-bc9b-66a1eb45882e"
+	.asciz	"d97c3989-c36f-44c3-b22d-8fbeb2453c69"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
