b29ed47395550718adf643ee3c088a8b6b143b62
zaidmelhem <zaidomar2011426@gmail.com>
2022-04-12 18:36:22 +0300

    add time

--- Barbar Salon/Barbar Salon/Views/AddTimePage.xaml.cs
+++ Barbar Salon/Barbar Salon/Views/AddTimePage.xaml.cs
@@ -12,9 +12,104 @@ namespace Barbar_Salon.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddTimePage : ContentPage
     {
+         DateTime dateTime;
+        DateTime date;
+        int Year, Month, Day;
         public AddTimePage()
         {
             InitializeComponent();
+            am.BackgroundColor = Color.Gray;
+                dateTime = DateTime.Now;
+
+                label.Text = dateTime.ToString("dddd, dd MMMM yyyy");
+                Year = DateTime.Now.Year;
+                Month = DateTime.Now.Month;
+                Day = DateTime.Now.Day;
+
         }
+
+        private void am_Clicked(object sender, EventArgs e)
+        {
+            pm.BackgroundColor = Color.White;
+            am.BackgroundColor = Color.Gray;
+            
+        }
+
+        private void pm_Clicked(object sender, EventArgs e)
+        {
+            am.BackgroundColor = Color.White;
+            pm.BackgroundColor= Color.Gray;
+            
+        }
+        
+    private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+    {
+    if (e.Value == true)
+    {
+      OffTime.IsVisible = true;
+    }
+    else
+    {
+      OffTime.IsVisible = false;
+    }
+    }
+
+        private void amE_Clicked(object sender, EventArgs e)
+        {
+            pmE.BackgroundColor = Color.White;
+            amE.BackgroundColor = Color.Gray;
+        }
+
+        private void pmE_Clicked(object sender, EventArgs e)
+        {
+            amE.BackgroundColor = Color.White;
+            pmE.BackgroundColor = Color.Gray;
+        }
+
+        private void amOs_Clicked(object sender, EventArgs e)
+        {
+            pmOs.BackgroundColor = Color.White;
+            amOs.BackgroundColor = Color.Gray;
+        }
+
+        private void pmOs_Clicked(object sender, EventArgs e)
+        {
+            amOs.BackgroundColor = Color.White;
+            pmOs.BackgroundColor = Color.Gray;
+        }
+
+        private void amOe_Clicked(object sender, EventArgs e)
+        {
+            pmOe.BackgroundColor = Color.White;
+            amOe.BackgroundColor = Color.Gray;
+        }
+
+        private void pmOe_Clicked(object sender, EventArgs e)
+        {
+            amOe.BackgroundColor = Color.White;
+            pmOe.BackgroundColor = Color.Gray;
+        }
+
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+    {
+    DateTime nowDate = new DateTime(Year, Month, Day);
+
+    var previewDate = nowDate.AddDays(-1);
+    label.Text = previewDate.ToString("dddd, dd MMMM yyyy");
+    Year = previewDate.Year;
+    Month = previewDate.Month;
+    Day = previewDate.Day;
+    }
+
+    private void ImageButton_Clicked_1(object sender, EventArgs e)
+    {
+    DateTime nowDate = new DateTime(Year, Month, Day);
+    var previewDate = nowDate.AddDays(1);
+    label.Text = previewDate.ToString("dddd, dd MMMM yyyy");
+    Year = previewDate.Year;
+    Month = previewDate.Month;
+    Day = previewDate.Day;
+           
+    }
     }
 }
--- Barbar Salon/Barbar Salon/obj/Debug/netstandard2.0/Views/AddTimePage.xaml.g.cs
+++ Barbar Salon/Barbar Salon/obj/Debug/netstandard2.0/Views/AddTimePage.xaml.g.cs
@@ -17,8 +17,52 @@ namespace Barbar_Salon.Views {
     public partial class AddTimePage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label label;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.ImageButton arrowleft;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button am;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button pm;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button amE;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button pmE;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.StackLayout OffTime;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button amOs;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button pmOs;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button amOe;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button pmOe;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(AddTimePage));
+            label = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "label");
+            arrowleft = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.ImageButton>(this, "arrowleft");
+            am = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "am");
+            pm = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "pm");
+            amE = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "amE");
+            pmE = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "pmE");
+            OffTime = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "OffTime");
+            amOs = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "amOs");
+            pmOs = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "pmOs");
+            amOe = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "amOe");
+            pmOe = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "pmOe");
         }
     }
 }
